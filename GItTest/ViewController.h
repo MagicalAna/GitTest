d8bb55b8babd24d2e33fec6aa02a343b40d153b9
your name <your_email@youremail.com>
2018-09-17 15:05:21 +0800

    Initial Commit

---
+++ GItTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GItTest
+//
+//  Created by Yuan Ana on 2018/9/17.
+//  Copyright © 2018 Yuan Ana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GItTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GItTest
+//
+//  Created by Yuan Ana on 2018/9/17.
+//  Copyright © 2018 Yuan Ana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
